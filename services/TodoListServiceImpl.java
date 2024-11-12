ec3d398ff6af91d2aca8f6cda7d264c1c3ae011a
Grace Eunice <2382025@unai.edu>
2024-11-12 20:03:02 +0700

    Update TodoListServiceImpl.java

--- services/TodoListServiceImpl.java
+++ services/TodoListServiceImpl.java
@@ -4,39 +4,38 @@ import entities.TodoList;
 import repositories.TodoListRepository;
 
 public class TodoListServiceImpl implements TodoListService {
-private final TodoListRepository todoListRepository;
+    private TodoListRepository todoListRepository;
 
-public TodoListServiceImpl(TodoListRepository todoListRepository){
-    this.todoListRepository = todoListRepository;
-}
-
-    @Override
-    public TodoList[] getTodoList() {
-        return new TodoList[0];
+    public TodoListServiceImpl(TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
     }
 
     @Override
-    public void addTodoList(String todo) {
-        if (todo.isEmpty() || todo.isBlank()) {
+    public TodoList[] getTodoList() {
+        return todoListRepository.getAll();
+    }
+
+    @Override
+    public void addTodoList(final String todo) {
+        if (todo.isBlank() || todo.isEmpty()) {
             System.out.println("Masukkan todo dengan benar");
             return;
         }
-
         TodoList todoList = new TodoList();
         todoList.setTodo(todo);
         todoListRepository.add(todoList);
     }
 
     @Override
-    public Boolean removeTodoList(Integer number) {
+    public Boolean removeTodoList(final Integer number) {
         return todoListRepository.remove(number);
     }
 
     @Override
-    public Boolean editTodoList(Integer number, String todo) {
-    TodoList todoList = new TodoList();
-    todoList.setTodo(todo);
-    todoList.setId(number);
+    public Boolean editTodoList(final Integer number, String todo) {
+        TodoList todoList = new TodoList();
+        todoList.setTodo(todo);
+        todoList.setId(number);
         return todoListRepository.edit(todoList);
     }
 }
